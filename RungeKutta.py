26c26f4bbda5657c0a9172e9027b4e2e115ffe2a
Alexander Shulzycki <AlexShulzycki@gmail.com>
2020-06-09 13:26:26 +0200

    updated rungekutta to plot

--- RungeKutta.py
+++ RungeKutta.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Function f, and step h, at starting position x0 and y0, up until x. Answer is at position (x, y(x)).
 def RungeKutta(functions, h, x0, y0, x):
@@ -18,14 +19,17 @@ def RungeKutta(functions, h, x0, y0, x):
             F3 = h * f(x0 + 0.5 * h, y0 + 0.5 * F2)
             F4 = h * f(x0 + h, y0 + F3)
 
-            x0 += h
             # y(x + h) = y(x) + 1/6 (F1 + 2F2 +2F3 +F4)
             y0 += ((1 / 6) * (F1 + 2 * F2 + 2 * F3 + F4))
 
             values.append([x0, y0])
+            # Increment x for next iteration
+            x0 += h
 
-        values_array = np.array(values)
         # PLOT results here so you get one plot per function
+        values_array = np.array(values)
+        plt.plot(values_array[:,0], values_array[:,1])
+        plt.show()
 
         results.append(y0)
 
--- test_RungeKutta.py
+++ test_RungeKutta.py
@@ -2,18 +2,19 @@ from unittest import TestCase
 import numpy
 import RungeKutta
 
+
 # You can hit the green arrows to run individual tests,
 # or press the arrow next to the class declaration to run all of them
 class Test(TestCase):
 
     def testRungeKuttaBasic(self):
         def f(x, y):
-            return (5 * x**2 - y)/(numpy.e**(x+y))
+            return (5 * x ** 2 - y) / (numpy.e ** (x + y))
 
         def g(x, y):
             return ((x + y) / 2)
+
         functions = [f]
         assert str(RungeKutta.RungeKutta(functions, 0.1, 1, 0, 5)[0])[0:6] == "2.1526"
         assert str(RungeKutta.RungeKutta(functions, 0.1, 0, 1, 0.1)[0])[0:6] == "0.9655"
         assert str(RungeKutta.RungeKutta(functions, 0.1, 0, 1, 0.2)[0])[0:6] == "0.9377"
-
