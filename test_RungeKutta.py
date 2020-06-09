4ba83cdfdaea89c8bf64d044497ab423f00556c5
Alexander Shulzycki <AlexShulzycki@gmail.com>
2020-06-09 12:19:22 +0200

    Removed superfluous tests

--- test_RungeKutta.py
+++ test_RungeKutta.py
@@ -6,22 +6,14 @@ import RungeKutta
 # or press the arrow next to the class declaration to run all of them
 class Test(TestCase):
 
-    def test_runge_kuttaTest(self):
-        assert RungeKutta.RungeKuttaTest(5) == 5
-        # Test passes because it returns true
-
-    def test(self):
-        # Create any function and it will run as a test - All assert
-        # statements must evaluate to true for the test pass.
-        assert 1 == 1
-        assert not False
-
     def testRungeKuttaBasic(self):
-        def f(x , y):
+        def f(x, y):
             return (5 * x**2 - y)/(numpy.e**(x+y))
+
+        def g(x, y):
+            return ((x + y) / 2)
+
         assert str(RungeKutta.RungeKutta(f, 0.1, 1, 0, 5))[0:6] == "2.1526"
-
-
-
-
+        assert str(RungeKutta.RungeKutta(f, 0.1, 0, 1, 0.1))[0:6] == "0.9655"
+        assert str(RungeKutta.RungeKutta(f, 0.1, 0, 1, 0.2))[0:6] == "0.9377"
 
